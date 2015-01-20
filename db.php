5373b2c8daa579560a524043de9c22c821cec4a2
Bobby Shirley <thebobbyshirley@gmail.com>
2015-01-20 11:42:34 -0600

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,11 @@
 <?php
+session_start();
 
-$sql_connection = mysql_connect("localhost", "pce_bobby", "Petrochem3");
-mysql_select_db("pce_app", $sql_connection) or die( "Unable to select database");
+$dbhost = "localhost"; // this will ususally be 'localhost', but can sometimes differ
+$dbname = "pce_app"; // the name of the database that you are going to use for this project
+$dbuser = "pce_bobby"; // the username that you created, or were given, to access your database
+$dbpass = "Petrochem3"; // the password that you created, or were given, to access your database
 
+mysql_connect($dbhost, $dbuser, $dbpass) or die("MySQL Error: " . mysql_error());
+mysql_select_db($dbname) or die("MySQL Error: " . mysql_error());
 ?>
